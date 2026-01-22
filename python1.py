cb905db8f980673cc597e281fcbceae6dd02ce74
Sylvain Le Bonniec <lebonniec.sylvain@gmail.com>
2026-01-22 14:51:29 +0000

    ajout du fichier python

---
+++ python1.py
@@ -0,0 +1,21 @@
+#sorted
+capitales=["Paris","Londres"]
+
+print("voici quelques capitales : ", capitales)
+
+position=input("entrez un chiffre entre 1 et 2 : ")
+classement=int(position) -1
+print(capitales[classement])
+
+Autre_capitale=input("entrez une capitale : ")
+capitales.append(Autre_capitale)
+Autre_capitale=input("entrez une capitale : ")
+capitales.append(Autre_capitale)
+Autre_capitale=input("entrez une capitale : ")
+capitales.append(Autre_capitale)
+print("liste capitales Mis à jour : ",capitales)
+
+capitales_triées=sorted(capitales)
+print("liste des capitales triées : ", capitales_triées)
+print("liste des capitales triées : ",sorted(capitales))
+print("liste des capitales triées inversée : ",sorted(capitales,reverse=True))
